011b80e4f1959c6520dbdbbb8d48abb65677069a
i-seo725 <hjing719@gmail.com>
2023-07-31 23:33:22 +0900

    쓸데없는 코드 지워서 화면전환 자연스러워짐

--- BookWarm/DetailViewController.swift
+++ BookWarm/DetailViewController.swift
@@ -19,8 +19,10 @@ class DetailViewController: UIViewController {
         detailLabel.text = "상세 화면"
         detailLabel.font = UIFont.systemFont(ofSize: 50)
         
-        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "lessthan"), style: .plain, target: self, action: #selector(backButtonTapped))
-        navigationItem.leftBarButtonItem?.tintColor = .black
+        // 이건 모달 방식일 때 하는 설정인듯!
+//        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "lessthan"), style: .plain, target: self, action: #selector(backButtonTapped))
+//        navigationItem.
+//        navigationItem.leftBarButtonItem?.tintColor = .black
         
         
     }
